4dcc16f21c575b5cb5d8f30620a251244da5bab0
khmo112233789 <khmo8192920@gmail.com>
2024-04-29 23:35:23 +0300

    modifying in js file

--- js/script.js
+++ js/script.js
@@ -207,78 +207,55 @@ const validateLoginInputs = (emailValue1, passwordValue1) => {
     return true;
 };
 
-
-
-
-
-
-
-
 // end login
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // start gellary
 
-// let btn1 = document.getElementById('btn1');
-// let btn2 = document.getElementById('btn2');
-// let btn3 = document.getElementById('btn3');
-// let btn4 = document.getElementById('btn4');
-// let btn5 = document.getElementById('btn5');
-// let btn6 = document.getElementById('btn6');
-// let btn7 = document.getElementById('btn7');
-// let btn8 = document.getElementById('btn8');
-// let btn9 = document.getElementById('btn9');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
+let btn7 = document.getElementById('btn7');
+let btn8 = document.getElementById('btn8');
+let btn9 = document.getElementById('btn9');
 
-// btn1.addEventListener('click',function(){
-//     location.assign('https://ar.wikipedia.org/wiki/%D8%AA%D9%85%D8%AB%D8%A7%D9%84_%D8%A7%D9%84%D8%AD%D8%B1%D9%8A%D8%A9')
-// })
+btn1.addEventListener('click',function(){
+    location.assign('https://ar.wikipedia.org/wiki/%D8%AA%D9%85%D8%AB%D8%A7%D9%84_%D8%A7%D9%84%D8%AD%D8%B1%D9%8A%D8%A9')
+})
 
-// btn2.addEventListener('click',function(){
-//     location.assign('https://www.ajnet.me/encyclopedia/2024/2/9/%D8%A8%D8%B1%D8%AC-%D8%A8%D9%8A%D8%B2%D8%A7-%D8%A7%D9%84%D9%85%D8%A7%D8%A6%D9%84')
-// })
+btn2.addEventListener('click',function(){
+    location.assign('https://www.ajnet.me/encyclopedia/2024/2/9/%D8%A8%D8%B1%D8%AC-%D8%A8%D9%8A%D8%B2%D8%A7-%D8%A7%D9%84%D9%85%D8%A7%D8%A6%D9%84')
+})
 
-// btn3.addEventListener('click',function(){
-//     location.assign('https://www.sis.gov.eg/Story/121619/%D8%A7%D9%84%D8%A3%D9%87%D8%B1%D8%A7%D9%85%D8%A7%D8%AA?lang=ar')
-// })
+btn3.addEventListener('click',function(){
+    location.assign('https://www.sis.gov.eg/Story/121619/%D8%A7%D9%84%D8%A3%D9%87%D8%B1%D8%A7%D9%85%D8%A7%D8%AA?lang=ar')
+})
 
-// btn4.addEventListener('click',function(){
-//     location.assign('https://www.un.org/ungifts/ar/%D8%A7%D9%84%D8%B3%D9%88%D8%B1-%D8%A7%D9%84%D8%B9%D8%B8%D9%8A%D9%85')
-// })
+btn4.addEventListener('click',function(){
+    location.assign('https://www.un.org/ungifts/ar/%D8%A7%D9%84%D8%B3%D9%88%D8%B1-%D8%A7%D9%84%D8%B9%D8%B8%D9%8A%D9%85')
+})
 
-// btn5.addEventListener('click',function(){
-//     location.assign('https://www.japan.travel/ar/spot/1691/')
-// })
+btn5.addEventListener('click',function(){
+    location.assign('https://www.japan.travel/ar/spot/1691/')
+})
 
-// btn6.addEventListener('click',function(){
-//     location.assign('https://ar.wikipedia.org/wiki/%D8%A8%D8%B1%D8%AC_%D8%AE%D9%84%D9%8A%D9%81%D8%A9')
-// })
+btn6.addEventListener('click',function(){
+    location.assign('https://ar.wikipedia.org/wiki/%D8%A8%D8%B1%D8%AC_%D8%AE%D9%84%D9%8A%D9%81%D8%A9')
+})
 
-// btn7.addEventListener('click',function(){
-//     location.assign('https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6')
-// })
+btn7.addEventListener('click',function(){
+    location.assign('https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6')
+})
 
-// btn8.addEventListener('click',function(){
-//     location.assign('https://ar.wikipedia.org/wiki/%D8%A5%D8%B3%D8%B7%D9%86%D8%A8%D9%88%D9%84')
-// })
+btn8.addEventListener('click',function(){
+    location.assign('https://ar.wikipedia.org/wiki/%D8%A5%D8%B3%D8%B7%D9%86%D8%A8%D9%88%D9%84')
+})
 
-// btn9.addEventListener('click',function(){
-//     location.assign('https://mawdoo3.com/%D9%85%D8%B9%D9%84%D9%88%D9%85%D8%A7%D8%AA_%D8%AD%D9%88%D9%84_%D8%AC%D8%B2%D8%B1_%D8%A7%D9%84%D9%85%D8%A7%D9%84%D8%AF%D9%8A%D9%81')
-// })
+btn9.addEventListener('click',function(){
+    location.assign('https://mawdoo3.com/%D9%85%D8%B9%D9%84%D9%88%D9%85%D8%A7%D8%AA_%D8%AD%D9%88%D9%84_%D8%AC%D8%B2%D8%B1_%D8%A7%D9%84%D9%85%D8%A7%D9%84%D8%AF%D9%8A%D9%81')
+})
 // end gellary
 
 
